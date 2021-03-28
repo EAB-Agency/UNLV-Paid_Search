8bc4a49c0474f973cb9ff42b7cfc73a92d4cbeaf
Andrei Olea <aolea@eab.com>
2021-03-28 09:27:17 -0400

    Images loading and Mobile optimization

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Layout, SEO } from "components/common";
-import { Link } from "gatsby";
-import { ModalRoutingContext } from "gatsby-plugin-modal-routing";
-import ContactForm from "components/landing/Contact/ContactForm";
+import React from 'react';
+import { Layout, SEO } from 'components/common';
+import { Link } from 'gatsby';
+import { ModalRoutingContext } from 'gatsby-plugin-modal-routing';
+import ContactForm from 'components/landing/Contact/ContactForm';
 
 export default () => (
   <Layout>
