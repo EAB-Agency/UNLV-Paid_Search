c1e0ed60a456507ecc655f3b6eceb1cf1602ae47
Bobby Smith <reversetype@gmail.com>
2021-03-30 06:37:07 -0400

    fix redirection

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,32 +3,38 @@ import { Layout, SEO } from 'components/common';
 import { Link } from 'gatsby';
 import { ModalRoutingContext } from 'gatsby-plugin-modal-routing';
 import { Contact } from 'components/landing';
+import { Redirect } from '@reach/router';
 
-export default () => (
-  <Layout>
-    <SEO />
-    <ModalRoutingContext.Consumer>
-      {/* On page reload, will not be a modal so will not show close link */}
-      {({ modal, closeTo }) => (
-        <>
-          {modal ? (
-            <div className="contact-header">
-              <Link
-                to={closeTo}
-                className="close-btn"
-                state={{
-                  noScroll: true,
-                }}
-              >
-                Close
-              </Link>
-              <Contact />
-            </div>
-          ) : (
-            (window.location.href = '/')
-          )}
-        </>
-      )}
-    </ModalRoutingContext.Consumer>
-  </Layout>
-);
+const RedirectLocation = () => (window.location.href = '/');
+
+export default () => {
+  const isBrowser = typeof window !== 'undefined';
+  return (
+    <Layout>
+      <SEO />
+      <ModalRoutingContext.Consumer>
+        {/* On page reload, will not be a modal so will not show close link */}
+        {({ modal, closeTo }) => (
+          <>
+            {modal ? (
+              <div className="contact-header">
+                <Link
+                  to={closeTo}
+                  className="close-btn"
+                  state={{
+                    noScroll: true,
+                  }}
+                >
+                  Close
+                </Link>
+                <Contact />
+              </div>
+            ) : (
+              isBrowser && <RedirectLocation />
+            )}
+          </>
+        )}
+      </ModalRoutingContext.Consumer>
+    </Layout>
+  );
+};
--- src/pages/help.js
+++ src/pages/help.js
@@ -3,37 +3,42 @@ import { Layout, SEO } from 'components/common';
 import { Link } from 'gatsby';
 import { ModalRoutingContext } from 'gatsby-plugin-modal-routing';
 
-export default () => (
-  <Layout>
-    <SEO title="UNLV Help" location="/help" />
-    <ModalRoutingContext.Consumer>
-      {/* On page reload, will not be a modal so will not show close link */}
-      {({ modal, closeTo }) => (
-        <>
-          {modal ? (
-            <div className="help">
-              <Link
-                to={closeTo}
-                className="close-btn"
-                state={{
-                  noScroll: true,
-                }}
-              >
-                Close
-              </Link>
-              <div className="head-content">
-                <h2>Help</h2>
-                <p>
-                  If you require assistance in using this site, please contact the Help Center at{' '}
-                  <a href="mailto:help@unlv.edu">help@unlv.edu.</a>
-                </p>
+const RedirectLocation = () => (window.location.href = '/');
+
+export default () => {
+  const isBrowser = typeof window !== 'undefined';
+  return (
+    <Layout>
+      <SEO title="UNLV Help" location="/help" />
+      <ModalRoutingContext.Consumer>
+        {/* On page reload, will not be a modal so will not show close link */}
+        {({ modal, closeTo }) => (
+          <>
+            {modal ? (
+              <div className="help">
+                <Link
+                  to={closeTo}
+                  className="close-btn"
+                  state={{
+                    noScroll: true,
+                  }}
+                >
+                  Close
+                </Link>
+                <div className="head-content">
+                  <h2>Help</h2>
+                  <p>
+                    If you require assistance in using this site, please contact the Help Center at{' '}
+                    <a href="mailto:help@unlv.edu">help@unlv.edu.</a>
+                  </p>
+                </div>
               </div>
-            </div>
-          ) : (
-            (window.location.href = '/')
-          )}
-        </>
-      )}
-    </ModalRoutingContext.Consumer>
-  </Layout>
-);
+            ) : (
+              isBrowser && <RedirectLocation />
+            )}
+          </>
+        )}
+      </ModalRoutingContext.Consumer>
+    </Layout>
+  );
+};
